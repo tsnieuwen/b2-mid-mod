5aaf18794c25355d8fa064e8850e035a191f1fdd
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-22 09:31:00 -0700

    Populate seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+mechanic1 = Mechanic.create(name: 'Mo', years_experience: 18)
+mechanic2 = Mechanic.create(name: 'Larry', years_experience: 10)
+mechanic3 = Mechanic.create(name: 'Curly', years_experience: 2)
+
+ride1 = Ride.create(name: 'Superman', thrill_rating: 9, open: true)
+ride2 = Ride.create(name: 'Plunger', thrill_rating: 4, open: false)
+ride3 = Ride.create(name: 'Tea Cups', thrill_rating: 1, open: false)
+ride4 = Ride.create(name: 'Twister', thrill_rating: 7, open: true)
+
+MechanicRide.create(mechanic: mechanic1, ride: ride1)
+MechanicRide.create(mechanic: mechanic2, ride: ride1)
+MechanicRide.create(mechanic: mechanic3, ride: ride1)
+MechanicRide.create(mechanic: mechanic2, ride: ride2)
+MechanicRide.create(mechanic: mechanic3, ride: ride2)
+MechanicRide.create(mechanic: mechanic3, ride: ride3)
+MechanicRide.create(mechanic: mechanic1, ride: ride4)
+MechanicRide.create(mechanic: mechanic3, ride: ride4)
